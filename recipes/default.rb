fda8e90b431fac6e045d7a73eac6507eca37fd4c
Tim Littlemore <tim@tjl2.com>
2011-10-08 15:39:50 +0100

    Added dashboard message and notify restart for nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,12 +6,22 @@ worker_count = 4
 # to passenger_monitor in MB
 memory_limit = 320
 
+ey_cloud_report "passenger" do
+  message "changing passenger settings"
+end
+
+service "nginx" do
+  supports :restart => true
+  action :enable
+end
+
 template "/etc/nginx/stack.conf" do
   owner node[:owner_name]
   group node[:owner_name]
   mode 0644
   source "stack.conf.erb"
   variables({:worker_count => worker_count})
+  notifies :restart, resources(:service => 'nginx')
 end
 
 cron "passenger_monitor_#{app_name}" do
