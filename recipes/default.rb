6b4a6a379c21786b4b2c5693db738df96e5e1d00
Tim Littlemore <tim@tjl2.com>
2011-10-08 16:01:01 +0100

    This should only run on app instances

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,33 +6,35 @@ worker_count = 4
 # to passenger_monitor in MB
 memory_limit = 320
 
-ey_cloud_report "passenger" do
-  message "changing passenger settings"
-end
+if ['app_master', 'app', 'solo'].include?(node[:instance_role])
+  ey_cloud_report "passenger" do
+    message "changing passenger settings"
+  end
 
-service "nginx" do
-  supports :restart => true
-  action :enable
-end
+  service "nginx" do
+    supports :restart => true
+    action :enable
+  end
 
-template "/etc/nginx/stack.conf" do
-  owner node[:owner_name]
-  group node[:owner_name]
-  mode 0644
-  source "stack.conf.erb"
-  variables({:worker_count => worker_count})
-  notifies :restart, resources(:service => 'nginx')
-end
+  template "/etc/nginx/stack.conf" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    source "stack.conf.erb"
+    variables({:worker_count => worker_count})
+    notifies :restart, resources(:service => 'nginx')
+  end
 
-cron "passenger_monitor_#{app_name}" do
-  action :delete
-end
+  cron "passenger_monitor_#{app_name}" do
+    action :delete
+  end
 
-cron "passenger_monitor_#{app_name}" do
-  minute  '*'
-  hour    '*'
-  day     '*'
-  month   '*'
-  weekday '*'
-  command "/usr/bin/lockrun --lockfile=/var/run/passenger_monitor_#{app_name}.lockrun -- /bin/bash -c '/engineyard/bin/passenger_monitor #{app_name} -l #{memory_limit} >/dev/null 2>&1'"
+  cron "passenger_monitor_#{app_name}" do
+    minute  '*'
+    hour    '*'
+    day     '*'
+    month   '*'
+    weekday '*'
+    command "/usr/bin/lockrun --lockfile=/var/run/passenger_monitor_#{app_name}.lockrun -- /bin/bash -c '/engineyard/bin/passenger_monitor #{app_name} -l #{memory_limit} >/dev/null 2>&1'"
+  end
 end
